ababca480a3bfd0c6c6477f8458741cd48641763
Your Name <you@example.com>
2023-03-13 17:45:50 -0400

    added  eedit event

--- index.js
+++ index.js
@@ -1,30 +1,66 @@
 let plus = document.querySelector(".fa-plus");
 let input = document.querySelector("#text-field");
-console.log(input.value);
+let section = document.querySelector("section");
+
+// edit.addEventListener("click", () => {
+//   console.log("editing");
+// });
+
+// console.log(input.value);
 const adding = () => {
+  let ul = document.createElement("ul");
+  ul.classList.add("data-container");
   plus.addEventListener("click", () => {
-    let div = document.createElement("d");
-    div.classList.add("container");
+    if (input.value) {
+      let div = document.createElement("d");
+      div.classList.add("container");
 
-    let ul = document.createElement("ul");
-    let li = document.createElement("li");
-    li.textContent = input.value;
+      let li = document.createElement("li");
+      li.classList.add("list-data");
 
-    let edit = document.createElement("i");
-    edit.classList.add("fa-solid");
-    edit.classList.add("fa-pen");
-    // console.log(edit);
-    li.appendChild(edit);
+      let h2 = document.createElement("h2");
+      h2.classList.add("data-item");
+      h2.textContent = input.value;
+      li.appendChild(h2);
 
-    let del = document.createElement("i");
-    del.classList.add("fa-regular");
-    del.classList.add("fa-trash-can");
+      let editBtn = document.createElement("button");
+      editBtn.classList.add("edit-btn");
+      //console.log(editBtn);
+      editBtn.addEventListener("click", () => {
+        let h2 = document.querySelector("h2");
+        input.value = h2.textContent;
+      });
 
-    li.appendChild(del);
-    ul.appendChild(li);
-    div.appendChild(ul);
-    // console.log(li);
-    console.log(div);
+      let editIcon = document.createElement("i");
+      editIcon.classList.add("fa-solid");
+      editIcon.classList.add("fa-pen");
+      editBtn.appendChild(editIcon);
+
+      let delBtn = document.createElement("button");
+      delBtn.classList.add("delete-btn");
+      let delIcon = document.createElement("i");
+      delIcon.classList.add("fa-regular");
+      delIcon.classList.add("fa-trash-can");
+      delBtn.appendChild(delIcon);
+
+      let btnDiv = document.createElement("div");
+      btnDiv.classList.add("btn-div");
+
+      btnDiv.appendChild(editBtn);
+      btnDiv.appendChild(delBtn);
+
+      li.appendChild(btnDiv);
+      //   li.appendChild(btnDiv)
+
+      ul.appendChild(li);
+      div.appendChild(ul);
+      console.log(li);
+      //   console.log(div);
+      section.appendChild(div);
+      input.value = "";
+    } else {
+      alert("you have to ente a task");
+    }
   });
 };
 
