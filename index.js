aa6c3c89ec274b0c045cf512d95b639f5c5fd8b9
Your Name <you@example.com>
2023-03-13 05:43:32 -0400

    created some divs and event listener

--- index.js
+++ index.js
@@ -1,4 +1,27 @@
 let plus = document.querySelector(".fa-plus");
-let inputValue = document.querySelector("input").value;
-console.log(inputValue);
-plus.addEventListener("click", () => console.log("you clicke on ", inputValue));
+let input = document.querySelector("#text-field");
+console.log(input.value);
+const adding = () => {
+  plus.addEventListener("click", () => {
+    let div = document.createElement("d");
+    div.classList.add("container");
+
+    let chilDiv = document.createElement("div");
+    chilDiv.textContent = input.value;
+    let edit = document.createElement("i");
+    edit.classList.add("fa-solid");
+    edit.classList.add("fa-pen");
+    // console.log(edit);
+    div.appendChild(chilDiv);
+    div.appendChild(edit);
+
+    let del = document.createElement("i");
+    del.classList.add("fa-regular");
+    del.classList.add("fa-trash-can");
+    // console.log(del);
+    div.appendChild(del);
+    console.log(div);
+  });
+};
+
+adding();
